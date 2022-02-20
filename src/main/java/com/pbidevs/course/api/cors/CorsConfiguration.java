ea8f6ae882ba76be63352d6be921656f66a49b5c
marcelosilva-dev <marceloeduardo244@gmail.com>
2022-02-20 20:02:04 -0300

    heroku deploy

--- src/main/java/com/pbidevs/course/api/cors/CorsConfiguration.java
+++ src/main/java/com/pbidevs/course/api/cors/CorsConfiguration.java
@@ -1,28 +1,20 @@
 package com.pbidevs.course.api.cors;
 
-import java.io.IOException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter implements Filter {
-
-	private String originPermitida = "http://clone-amazon-vuejs.s3-website.us-east-2.amazonaws.com";
+	// private String originPermitida = "http://clone-amazon-vuejs.s3-website.us-east-2.amazonaws.com";
 	
 	
 	// LIBERADO ACESSO DE CORS PARA APENAS UM DOMINIO
@@ -36,7 +28,7 @@ public class CorsFilter implements Filter {
     } */
 
     // CASO QUERIA FILTRAR APENAS UM DOMINIO
-    @Override
+    /* @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
             throws IOException, ServletException {
         		
@@ -54,8 +46,8 @@ public class CorsFilter implements Filter {
 			response.setStatus(HttpServletResponse.SC_OK);
 		} else {
 			chain.doFilter(req, resp);
-		}
+		} 
         
-    }
+    } */
     
 }
